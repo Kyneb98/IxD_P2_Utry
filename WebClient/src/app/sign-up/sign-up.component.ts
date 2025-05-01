26006d961efa888ba625f0e95b595ac6e606c3c7
Kyneb98 <tkwa22@student.aau.dk>
2025-05-01 11:41:41 +0200

    kk

--- WebClient/src/app/sign-up/sign-up.component.ts
+++ WebClient/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @Component({
   selector: 'app-sign-up',
